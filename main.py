43069bc141733f0fc9d283d8cf8e8c320ea96d72
unknown <nadaamrr507@gmail.com>
2026-07-26 18:22:36 +0300

    read endpoints with 404

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+tasks = [
+    {"id": 1, "title":"docker course", "done":True},
+    {"id": 2, "title":"kubernetes course", "done":True},
+    {"id": 3, "title":"GitLab course", "done":False}
+]
+
 @app.get("/")
 async def root():
     return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
@@ -10,3 +16,17 @@ async def root():
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+@app.get("/tasks")
+async def display_tasks():
+    return tasks
+
+from fastapi import HTTPException
+
+@app.get("/tasks/{id}")
+async def display_task(id:int):
+    for task in tasks:
+        if(id==task["id"]):
+            return task
+    raise HTTPException(status_code=404, detail=f"Task {id} not found")
+    
